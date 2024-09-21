8488489628353436e45f6a1fe2cea06d485bfcd6
romaingaget <romaingaget@gmail.com>
2024-09-21 19:07:26 +0100

    initial commit

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+    return (
+        <div>
+            <h2>Search for a City</h2>
+            <input type="text" placeholder="Enter a city name" />
+            <button>Search</button>
+        </div>
+    );
+}
---
+++ src/Components/WeatherDisplay.jsx
@@ -0,0 +1,11 @@
+function WeatherDisplay() {
+    return (
+        <div>
+            <h2>Weather Information</h2>
+            <p>City: </p>
+            <p>Temperature: </p>
+            <p>Condition: </p>
+        </div>
+    );
+}
+export default WeatherDisplay;
